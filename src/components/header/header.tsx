375ed673e154083cef60f15d4fb172553a306cb1
bircankut <bircankut@gmail.com>
2023-07-07 16:24:25 +0300

    feat: added headers to header section

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -6,7 +6,13 @@ interface HeaderProps {
 }
 
 const Header: FC<HeaderProps> = ({}) => {
-  return <div className={style.headerBox}>header</div>
+  return <div className={style.headerBox}>
+    <h2 className={`${style.fSize46} fontLato colorDarkGrey`}>
+      Expence Calculater
+    </h2>
+    <h4 className={`${style.mTop10} fontLato colorDarkGrey`}>List of Expences</h4>
+
+  </div>
 }
 
 export { Header }
