0af811df5de9020a7ab0db53c5e23c6dce705d6c
bircankut <bircankut@gmail.com>
2023-07-07 14:18:32 +0300

    feat: added list section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css'
-import { Header } from '../src/components/header/header'
+import './App.css';
+import { Header } from '../src/components/header/header';
+import { ListSection } from '../src/components/list-section/list-section'
 
 function App() {
   return (
     <div className='main'>
       <Header/>
+      <ListSection/>
       </div>
   )
 }
---
+++ src/components/list-section/list-section.tsx
@@ -0,0 +1,12 @@
+import { FC } from 'react'
+import style from '../list-section/list-section.module.css'
+
+interface ListSectionProps {
+  
+}
+
+const ListSection: FC<ListSectionProps> = ({}) => {
+  return <div className={style.listSectionBox}>listSection</div>
+}
+
+export { ListSection }
