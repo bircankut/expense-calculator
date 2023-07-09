03529322ce58338e12893922d079f83ef74dbb81
bircankut <bircankut@gmail.com>
2023-07-09 18:01:45 +0300

    feat: added expence array

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import './App.css';
 import { Header } from '../src/components/header/header';
 import { ExpenceList } from './components/expence-list/expence-list.tsx';
+import { useState } from 'react';
+
+interface Expence {
+  name: string;
+  price: number;
+  percentageMarkup: number;
+  total: () => {};
+}
+
 
 function App() {
+  const [expences, setExpences] = useState<Expence[]>([]);
+  const [showModal, setShowModal] = useState(false);
+
   return (
     <div className='main'>
-      <Header/>
+      <Header showModal={showModal} setShowModal={setShowModal}/>
       <ExpenceList/>
       </div>
   )
