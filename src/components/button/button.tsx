4214b533d747843341d6cd9aad3625c58e2e51ab
bircankut <bircankut@gmail.com>
2023-07-09 22:07:10 +0300

    feat: added icon button class

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,15 +1,18 @@
-import { Children, FC } from 'react'
+import { FC } from 'react'
 import style from '../button/button.module.css'
+import { COLOR } from '../../enums';
 
 interface ButtonProps {
   showModal: boolean;
   setShowModal: (value: boolean) => void;
   children: string;
+  color?: string;
+  iconButton?: boolean;
 }
 
-const Button: FC<ButtonProps> = ({children, showModal, setShowModal}) => {
+const Button: FC<ButtonProps> = ({children, showModal, setShowModal, color, iconButton}) => {
    
-  return <button className={`${style.button} fontLato`} onClick={()=> setShowModal(!showModal)}>
+  return <button style={{ backgroundColor: `${color}` }} className={iconButton ? `${style.iconButton} fontLato` :`${style.button} fontLato`} onClick={()=> setShowModal(!showModal)}>
     {children}
     </button>
 }
