65189ec3dac66a194bce299897337d1d1f571a21
bircankut <bircankut@gmail.com>
2023-07-09 09:20:45 +0300

    feat: added style to button component

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react'
+import style from '../button/button.module.css'
 
 interface ButtonProps {
-  
+  text: String
 }
 
-const Button: FC<ButtonProps> = ({}) => {
-  return <div>button</div>
+const Button: FC<ButtonProps> = ({text}) => {
+  return <div className={`${style.button} fontLato`}>{text}</div>
 }
 
 export { Button }
