f20f54811065c33d1b00230a6477867f66a9188c
bircankut <bircankut@gmail.com>
2023-07-09 19:08:04 +0300

    feat: added input elements to modal component

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,11 +1,24 @@
 import { FC } from 'react'
+import style from '../modal/modal.module.css';
+import { Button } from '../button/button';
 
 interface ModalProps {
-  
+  showModal: boolean;
+  setShowModal: (value: boolean) => void
 }
 
-const Modal: FC<ModalProps> = ({}) => {
-  return <div>modal</div>
+const Modal: FC<ModalProps> = ({showModal, setShowModal}) => {
+  return <div className={style.darkBG}>
+    <div className={style.centered}>
+      <div className={style.modal}>
+      <Button showModal={showModal} setShowModal={setShowModal}>Close</Button>
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      <input type="text" />
+      </div>
+    </div>
+  </div>
 }
 
 export { Modal }
