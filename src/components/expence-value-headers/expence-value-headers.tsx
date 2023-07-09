8e761f9e6a8c47022c24df5be614d1fba96ecf3f
bircankut <bircankut@gmail.com>
2023-07-09 13:57:47 +0300

    feat: added new css classes to expence-value-headers component

--- src/components/expence-value-headers/expence-value-headers.tsx
+++ src/components/expence-value-headers/expence-value-headers.tsx
@@ -1,11 +1,13 @@
 import { FC } from 'react'
+import style from '../expence-value-headers/expence-value-headers.module.css'
 
 interface ExpenceValueHeadersProps {
   children: string;
+  wideness: string;
 }
 
-const ExpenceValueHeaders: FC<ExpenceValueHeadersProps> = ({children}) => {
-  return <div>{children}</div>
+const ExpenceValueHeaders: FC<ExpenceValueHeadersProps> = ({children, wideness}) => {
+  return <div className={style.expenceValueHeader} style={{width: `${wideness}`}}>{children}</div>
 }
 
 export { ExpenceValueHeaders }
