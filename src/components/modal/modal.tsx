536d4ed4383df5fb6ced995297b803ca29f189cd
bircankut <bircankut@gmail.com>
2023-07-10 09:29:42 +0300

    feat: added calculation function

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -3,50 +3,67 @@ import style from '../modal/modal.module.css';
 import { Button } from '../button/button';
 import { COLOR } from '../../enums';
 import { Expence } from '../../App';
+import BigNumber from 'bignumber.js';
 
 interface ModalProps {
   showModal: boolean;
   setShowModal: (value: boolean) => void;
   wipExpence: Partial<Expence>;
-  handleWipExpenceChange: (value: Partial<Expence>)=> void
+  handleWipExpenceChange: (value: Partial<Expence>) => void;
+  intertToExpences: (expence: Expence) => void;
 }
 
-const Modal = ({showModal, setShowModal, wipExpence, handleWipExpenceChange}: ModalProps) => {
+const Modal = ({ showModal, setShowModal, wipExpence, handleWipExpenceChange, intertToExpences }: ModalProps) => {
 
   function handleNameChange(e: any) {
     handleWipExpenceChange({
-      name: e.target.value, 
+      name: e.target.value,
       price: wipExpence.price,
       percentageMarkup: wipExpence.percentageMarkup,
     });
   };
   function handlePriceChange(e: any) {
     handleWipExpenceChange({
-      name: wipExpence.name, 
+      name: wipExpence.name,
       price: e.target.value,
       percentageMarkup: wipExpence.percentageMarkup,
     });
   };
   function handlePercentageChange(e: any) {
     handleWipExpenceChange({
-      name: wipExpence.name, 
+      name: wipExpence.name,
       price: wipExpence.price,
       percentageMarkup: e.target.value,
     });
   };
 
+  function calculate(pr: number, per: number): Partial<Expence> {
+    const price = new BigNumber(pr);
+    const percentage = new BigNumber(per);
+    const mult = price.multipliedBy(percentage);
+    const total = price.plus(mult);
+    return {
+      name: wipExpence.name,
+      price: wipExpence.price,
+      percentageMarkup: wipExpence.percentageMarkup,
+      total,
+    }
+  }
+
+  function addExpence(value: Partial<Expence>) {
+    const expence = calculate(value.price!, value.percentageMarkup!);
+    intertToExpences(expence as Expence);
+  }
+
   return <div className={style.darkBG}>
     <div className={style.centered}>
       <div className={style.modal}>
-        <Button iconButton={true} color={COLOR['grey']} showModal={showModal} setShowModal={setShowModal}>
-          X
-        </Button>
         <div className={style.inputBox}>
           <h3 className={`${style.modalHeader} fontLato colorDarkGrey`}>Adding New Expence</h3>
-          <input className={`${style.input} fontLato`} type="text" value={wipExpence.name} onChange={handleNameChange} placeholder='Name'/>
-          <input className={`${style.input} fontLato`}  type="text" value={wipExpence.price} onChange={handlePriceChange}  placeholder='Price'/>
-          <input className={`${style.input} fontLato`}  type="text" value={wipExpence.percentageMarkup} onChange={handlePercentageChange}  placeholder='Percentage Markup'/>
-          <Button color={COLOR['grey']} showModal={showModal} setShowModal={setShowModal}>
+          <input className={`${style.input} fontLato`} type="text" value={wipExpence.name} onChange={handleNameChange} placeholder='Name' />
+          <input className={`${style.input} fontLato`} type="text" value={wipExpence.price} onChange={handlePriceChange} placeholder='Price' />
+          <input className={`${style.input} fontLato`} type="text" value={wipExpence.percentageMarkup} onChange={handlePercentageChange} placeholder='Percentage Markup' />
+          <Button color={COLOR['grey']} onClick={() => { addExpence(wipExpence) }}>
             Add an Expence
           </Button>
         </div>
