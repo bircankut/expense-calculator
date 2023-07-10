fc3a9212f9d180969d4f386f8631bb1f91c7051c
bircankut <bircankut@gmail.com>
2023-07-10 15:10:39 +0300

    feat: added listing logic

--- src/components/expence-box/expence-box.tsx
+++ src/components/expence-box/expence-box.tsx
@@ -1,15 +1,21 @@
 import { FC, useState } from 'react'
 import BigNumber from "bignumber.js";
-
+import { Expence } from '../../App';
+import style from '../expence-box/expence-box.module.css';
 
 interface ExpenceBoxProps {
-  
+  expences: Expence[];
 }
 
-
-const ExpenceBox = ({
-}:ExpenceBoxProps) => {
-  return <div>expence-box</div>
+const ExpenceBox = ({ expences }:ExpenceBoxProps) => {
+  return <div>{expences.map((el)=>{
+    return (<div className={`${style.expenceBox} fontLato colorLigthGrey`}>
+        <span style={{width: '30%'}} className={style.span}>{el.name}</span>
+        <span style={{width: '25%'}} className={style.span}>{el.price}€</span>
+        <span style={{width: '10%'}} className={style.span}>{el.percentageMarkup}%</span>
+        <span style={{width: '30%'}} className={style.span}>{el.total?.toString()}€</span>
+    </div>)
+  })}</div>
 }
 
 export { ExpenceBox }
