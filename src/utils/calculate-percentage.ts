76f15d45c9b95ccf181256de3a9e7a46105ef6a7
bircankut <bircankut@gmail.com>
2023-07-13 16:16:36 +0300

    feat: added calculate function for expence total

---
+++ src/utils/calculate-percentage.ts
@@ -0,0 +1,11 @@
+import BigNumber from "bignumber.js";
+
+export const calculateValueRatio = (value?: number, ratio?: number): BigNumber => {
+    if (!value || !ratio) {
+        return new BigNumber(0);
+    }
+
+    const valueAsBigNumber = new BigNumber(value);
+    const ratioAsBigNumber = new BigNumber(ratio);
+    return valueAsBigNumber.multipliedBy(ratioAsBigNumber.plus(100)).dividedBy(100);
+};
